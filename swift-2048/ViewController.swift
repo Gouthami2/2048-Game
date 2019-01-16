ff78ae3a8fbbf4c9fb72817b5b5cb7d50e1205b2
Gouthami2 <gouthamireddy1223@gmail.com>
2019-01-16 18:33:20 -0500

    initial commit

---
+++ swift-2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-2048
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  @IBAction func startGameButtonTapped(_ sender : UIButton) {
+    let game = NumberTileGameViewController(dimension: 4, threshold: 2048)
+    self.present(game, animated: true, completion: nil)
+  }
+}
+
